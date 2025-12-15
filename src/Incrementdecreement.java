01a94026072e0b054eecc4a13aa9700474953dbd
username <sdet.m.meenakshi@gmail.com>
2025-12-15 21:47:55 +0530

    Hi 3 attempt ......

---
+++ src/Incrementdecreement.java
@@ -0,0 +1,21 @@
+public class Incrementdecreement {
+    public static void main(String[] args) {
+        int i=20;
+        int j=2;
+        System.out.println("----PreIncrement----");
+        System.out.printf("value of i=%d\n",i);
+        System.out.printf("value of j=%d",++i);
+        System.out.println("----PostIncrement----");
+        System.out.printf("value of i=%d\n",i);
+        System.out.printf("value of i=%d\n",i++);
+
+        System.out.println("----PredecremenT----");
+        System.out.printf("value of i=%d\n",i);
+        System.out.printf("value of i=%d",--i);
+        System.out.println("----PredecremenT----");
+        System.out.printf("value of i=%d\n",i);
+        System.out.printf("value of i=%d",i--);
+
+
+    }
+}
