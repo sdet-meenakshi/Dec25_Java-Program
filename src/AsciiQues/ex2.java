5a230d571bf2a724cb7ac81073c47a42406a0be8
username <sdet.m.meenakshi@gmail.com>
2025-12-15 22:13:38 +0530

    LeapYear

---
+++ src/AsciiQues/ex2.java
@@ -0,0 +1,9 @@
+package AsciiQues;
+
+public class ex2 {
+    public static void main(String[] args) {
+        short S=10;
+        char a1='A';// ascii value of a is 65
+        System.out.println(S+a1);
+    }
+}
--- src/Pr1/test3.java
+++ src/Pr1/test3.java
@@ -1,4 +1,13 @@
 package Pr1;
 
 public class test3 {
+    public static void main(String[] args) {
+        byte a=10;
+        short b=20;
+        char A='B';
+        long C=12345L;// can use L IN BOTH WAYS cap anD small both
+        float D= 23.56F;// can use F IN BOTH WAYS cap anD small both
+        double E= 345.897;
+        String S="Meenakshi";
+    }
 }
